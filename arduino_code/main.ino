f0ef7d272cb7e2cb2f8648cc36c6ab2d8f2b0bb8
jirivchi1 <jiriverachinchuna@al.uloyola.es>
2024-09-19 12:03:58 +0200

    PC: add main.ino

--- arduino_code/main.ino
+++ arduino_code/main.ino
@@ -1,43 +1,65 @@
-#include "LowPower.h"
-const int RELAY_PIN = 7;  // the Arduino pin, which connects to the IN pin of relay
-const int ledpin = 13;
-int currentHour;
+#include <LowPower.h>
+
+const int RELAY_PIN = 7;  // Pin del Arduino conectado al relé
+
+// Configuración de tiempos
+const unsigned long ACTIVATION_TIME = 120000;  // 2 minutos en milisegundos
+const unsigned long EXTENDED_TIME = 240000;    // 4 minutos en milisegundos
+const unsigned long SLEEP_TIME = 21600000;     // 6 horas en milisegundos
+const unsigned long NIGHT_SLEEP_TIME = 43200000; // 12 horas en milisegundos (ajustable)
+
+int photoCount = 0;
+bool isDaytime = true;
+
 void setup() {
   pinMode(RELAY_PIN, OUTPUT);
-  pinMode(ledpin, OUTPUT);
-  Serial.begin(9600);  // Inicia la comunicación serial
+  digitalWrite(RELAY_PIN, LOW);  // Asegurarse de que el relé está apagado al inicio
 }
- 
+
 void loop() {
-  digitalWrite(RELAY_PIN, HIGH);
-  digitalWrite(ledpin,HIGH);
- 
- 
- if (Serial.available() > 0) {
-    // Leer la hora enviada desde la Raspberry Pi
-    currentHour = Serial.parseInt();
-    Serial.println(currentHour);
-    delay(5000);
-    // Condiciones para encender o apagar el LED
-    if (currentHour >= 6 && currentHour < 12) {
-      delay(10000);
-      digitalWrite(RELAY_PIN, LOW);
-      digitalWrite(ledpin,LOW);
-      for(int j=0;j<1;j++) LowPower.powerDown(SLEEP_8S,ADC_OFF,BOD_OFF);
-      Serial.println("sleeping 8s");
- 
-    } else {
-      delay(1000);
-      digitalWrite(RELAY_PIN, LOW);
-      for(int j=0;j<2;j++) LowPower.powerDown(SLEEP_8S,ADC_OFF,BOD_OFF);
-      Serial.println("sleeping 16s");
+  if (isDaytime) {
+    takePhoto();
+    photoCount++;
+    
+    if (photoCount >= 4) {
+      switchToNight();
     }
+  } else {
+    sleepNight();
+    switchToDay();
   }
- 
-  else{
-    delay(5000);
-    digitalWrite(RELAY_PIN, LOW);
-    delay(5000);
-    Serial.println("no hay conexion");
+}
+
+void takePhoto() {
+  digitalWrite(RELAY_PIN, HIGH);
+  
+  if (photoCount == 3) {
+    // Última foto del ciclo, mantener encendido por 4 minutos
+    delay(EXTENDED_TIME);
+  } else {
+    delay(ACTIVATION_TIME);
   }
- 
+  
+  digitalWrite(RELAY_PIN, LOW);
+  
+  // Dormir entre fotos
+  for (unsigned long i = 0; i < SLEEP_TIME; i += 8000) {
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+  }
+}
+
+void switchToNight() {
+  isDaytime = false;
+  photoCount = 0;
+}
+
+void switchToDay() {
+  isDaytime = true;
+}
+
+void sleepNight() {
+  // Dormir durante toda la noche
+  for (unsigned long i = 0; i < NIGHT_SLEEP_TIME; i += 8000) {
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+  }
+}
