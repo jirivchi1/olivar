e6501f9424b2b03aec5969e5696614cbf86a4293
Ismael Rivera <117596961+jirivchi1@users.noreply.github.com>
2024-09-30 17:19:47 +0200

    Update main.py log file

--- nodo_banda/main.py
+++ nodo_banda/main.py
@@ -71,7 +71,7 @@ def delete_photos():
 
 
 def log_action(message):
-    with open(f"{METRICS_DIRECTORY}/log.txt", "a") as log_file:
+    with open(f"{METRICS_DIRECTORY}/log_banda.txt", "a") as log_file:
         log_file.write(f"{datetime.now()}: {message}\n")
 
 
